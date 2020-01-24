234256813ff29e839bebb7776afefb78e9ee1259
Ashiqur05 <rahmanashiqur41@yahoo.com>
2020-01-24 14:50:28 +0100

    prediction

--- Predicting Sentiments.R
+++ Predicting Sentiments.R
@@ -1,5 +1,8 @@
 
 
+iphoneRC 
+
+galaxyRC 
 
 # load the model for iphone
 iphone_model_RF <- readRDS("final_model_RF_iphone.rds")
@@ -12,3 +15,22 @@ print(galaxy_model_RF)
 
 # load large Dataset
 largematrix<-read.csv("sentiment analysis/LargeMatrix.csv")
+summary(largematrix)
+str(largematrix)
+dim(largematrix)
+
+#iphone prediction
+pred_iphone_final <- predict(iphone_model_RF, newdata = largematrix,type = "raw")
+summary(pred_iphone_final)
+
+# probability comparison
+prop.table(table(pred_iphone_final))
+prop.table(table(iphoneRC$iphonesentiment))
+
+#galaxy prediction
+pred_galaxy_final <- predict(galaxy_model_RF, newdata = largematrix,type = "raw")
+summary(pred_galaxy_final)
+
+# probability comparison
+prop.table(table(pred_galaxy_final))
+prop.table(table(galaxyRC$galaxysentiment))
